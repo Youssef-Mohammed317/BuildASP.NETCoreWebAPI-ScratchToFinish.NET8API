945f635d9174768709378e711d82f1c2009c5fb1
Youssef-Mohammed317 <youssefmohammad040317@gmail.com>
2025-09-27 18:19:35 +0300

    End of Section 2 using Clean Architecture add models to domain and context to infra.data project and inject context in api program and mad ioc container class and use it in api program and add migration then update data base

---
+++ ZNWalks/ZNWalk.Infa.Data/Contexts/ZNWalksDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZNWalks.Domain.Models;
+
+namespace ZNWalk.Infa.Data.Contexts
+{
+    public class ZNWalksDbContext : DbContext
+    {
+        public ZNWalksDbContext() : base()
+        {
+        }
+        public ZNWalksDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Difficulty> Difficulty { get; set; }
+        public DbSet<Region> Region { get; set; }
+        public DbSet<Walk> Walk {  get; set; }
+    }
+}
--- ZNWalks/ZNWalks.Api/Program.cs
+++ ZNWalks/ZNWalks.Api/Program.cs
@@ -1,4 +1,9 @@
 
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+using ZNWalk.Infa.Data.Contexts;
+using ZNWalks.Infra.IoC;
+
 namespace ZNWalks.Api
 {
     public class Program
@@ -13,6 +18,15 @@ namespace ZNWalks.Api
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+
+            builder.Services.AddDbContext<ZNWalksDbContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("ZNWalksConnectionString"));
+            });
+
+
+            RegisterServices(builder.Services);
 
             var app = builder.Build();
 
@@ -31,6 +45,10 @@ namespace ZNWalks.Api
             app.MapControllers();
 
             app.Run();
+        }
+        public static void RegisterServices(IServiceCollection services)
+        {
+            DependancyContainer.RegisterServices(services);
         }
     }
 }
