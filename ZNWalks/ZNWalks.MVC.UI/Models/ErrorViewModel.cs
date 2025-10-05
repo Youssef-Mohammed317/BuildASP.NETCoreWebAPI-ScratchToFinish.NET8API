50fc26867635ae8915b45f54c22081a6481d6b66
Youssef-Mohammed317 <youssefmohammad040317@gmail.com>
2025-10-05 20:09:02 +0300

    remove ui folder

--- ZNWalks/ZNWalks.MVC.UI/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ZNWalks.MVC.UI.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
