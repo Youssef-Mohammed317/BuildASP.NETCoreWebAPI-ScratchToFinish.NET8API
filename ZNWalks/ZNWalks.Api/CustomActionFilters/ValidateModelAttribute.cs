8d244fea8136da8bfa011939d863ce0e256c4526
Youssef-Mohammed317 <youssefmohammad040317@gmail.com>
2025-10-08 22:40:44 +0300

    adjust vaildatemodel filter

--- ZNWalks/ZNWalks.Api/CustomActionFilters/ValidateModelAttribute.cs
+++ ZNWalks/ZNWalks.Api/CustomActionFilters/ValidateModelAttribute.cs
@@ -7,6 +7,9 @@ namespace ZNWalks.Api.CustomActionFilters
     {
         public override void OnActionExecuting(ActionExecutingContext context)
         {
+            if(!context.ModelState.IsValid)
+            {
+
             var errors = context.ModelState
                 .Where(ms => ms.Value?.Errors?.Count > 0)
                 .Select(ms => new
@@ -14,18 +17,24 @@ namespace ZNWalks.Api.CustomActionFilters
                     Field = ms.Key,
                     Errors = ms.Value?.Errors?.Select(e => e.ErrorMessage).ToArray()
                 });
-            if (errors.Any())
-            {
 
-                var response = new
+                if (errors.Any())
                 {
-                    Success = false,
-                    Message = "Validation failed.",
-                    Errors = errors
-                };
+                    var response = new
+                    {
+                        Success = false,
+                        Message = "Validation failed.",
+                        Errors = errors
+                    };
 
-                context.Result = new BadRequestObjectResult(response);
+                    context.Result = new BadRequestObjectResult(response);
+                }
             }
+
+        
+
+
+            
         }
     }
 }
